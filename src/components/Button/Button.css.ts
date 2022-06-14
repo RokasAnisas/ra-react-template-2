61fdb4ca73d3338ae70c46dc3c4e87a1f9c5a99b
Rokas Anisas <rokas.anisas@gmail.com>
2022-06-14 12:49:28 +0300

    Update Button.css.ts

--- src/components/Button/Button.css.ts
+++ src/components/Button/Button.css.ts
@@ -2,8 +2,11 @@ import { style } from '@vanilla-extract/css';
 
 export const button = style({
   display: 'flex',
+  justifyContent: 'center',
   backgroundColor: '#59A96A',
   border: 0,
+  borderRadius: '10px',
+  minWidth: '4rem',
   padding: '10px',
   transition: 'all 0.3s ease',
   ':hover': {
