d0cc5721e79fb546315089feb5def2998d78fb2b
Rokas Anisas <rokas.anisas@gmail.com>
2022-07-14 14:29:26 +0300

    theme switcher button

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import { FC } from 'react';
 
 import { Example } from '@/features/example/components/Example';
 import { useApplyTheme } from '@/features/themeSwitcher/themeSwitcher.hooks';
-import { toggleTheme } from '@/features/themeSwitcher/themeSwitcher.slice';
-import { useAppDispatch } from '@/middleware/redux/redux.hooks';
+import { ThemeSwitcherButton } from '@/features/themeSwitcher/components/ThemeSwitcherButton';
 
 const App: FC = () => {
-  const dispatch = useAppDispatch();
   useApplyTheme();
 
   return (
     <>
-      <button onClick={() => dispatch(toggleTheme())}>Toggle theme</button>
+      <ThemeSwitcherButton />
       <Example />
     </>
   );
