dffd11e5db4f1273dd6fba03d8862f15f8099e32
Rokas Anisas <rokas.anisas@gmail.com>
2022-07-14 12:41:29 +0300

    add sass

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
+import classNames from 'classnames/bind';
 
 import { ButtonProps } from './Button.props';
-import { button, label } from './Button.css';
+
+import style from './Button.module.scss';
+const cx = classNames.bind(style);
 
 export const Button: FC<ButtonProps> = ({ children, onClick }) => {
+  const className = 'button';
+
   return (
-    <button className={button} onClick={onClick}>
-      <span className={label}>{children}</span>
+    <button className={cx(className)} onClick={onClick}>
+      <span className={cx(`${className}__label`)}>{children}</span>
     </button>
   );
 };
