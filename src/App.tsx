bc999f9f8fc8af30fbb4434c927492ae70501baf
Rokas Anisas <rokas.anisas@gmail.com>
2022-11-13 13:13:35 +0200

    router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { FC } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import { ThemeSwitcherButton } from '@/modules/themeSwitcher/components/ThemeSwitcherButton';
-
-import { HomeRoute } from './routes/Home.route';
-import { routes } from './config/react-router/routes';
+import { HomeRoute } from '@/routes/Home.route';
+import { routes } from '@/config/react-router/routes';
+import { RouterProvider } from '@/config/react-router/router.provider';
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={routes.home} element={<HomeRoute />} />
-        <Route path="/theme" element={<ThemeSwitcherButton />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider>
+      <Route path={routes.home} element={<HomeRoute />} />
+      <Route path="/theme" element={<ThemeSwitcherButton />} />
+    </RouterProvider>
   );
 };
 
---
+++ src/config/react-router/router.provider.tsx
@@ -0,0 +1,12 @@
+import { FC, ReactNode } from 'react';
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+interface Props {
+  children: ReactNode;
+}
+
+export const RouterProvider: FC<Props> = ({ children }) => (
+  <BrowserRouter>
+    <Routes>{children}</Routes>
+  </BrowserRouter>
+);
